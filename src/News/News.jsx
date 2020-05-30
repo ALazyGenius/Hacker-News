d5578fb98987513e3a8acfd1bbf570b45c2c741e
AkashMishraRF <akmishra@rodanandfields.com>
2020-05-30 16:18:40 +0530

    Table CHnages

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import './News.css';
+import React from "react";
+import "./News.css";
 
 const News = () => {
-
-
-
-    return (
-        <div></div>
-    );
-
-}
+  return (
+    <div className="news-container">
+      <div className="card">
+        <div class="card-header">Featured</div>
+        <div className="card-body">
+          <div className="story_container">
+            <div className="story_data">
+              <div className="story_title">
+                <a href="https://news.ycombinator.com/item?id=16582136">
+                  <span>Stephen Hawking has died</span>
+                </a>
+                <a
+                  href="http://www.bbc.com/news/uk-43396008"
+                  target="_blank"
+                  className="story_link"
+                >
+                  (http://www.bbc.com/news/uk-43396008)
+                </a>
+              </div>
+              <div className="story_meta">
+                <span>
+                  <a href="https://news.ycombinator.com/item?id=16582136">
+                    6015 points
+                  </a>
+                </span>
+                ,<span className="story_separator">|</span>
+                <span>
+                  <a href="https://news.ycombinator.com/user?id=Cogito">
+                    <span>Cogito</span>
+                  </a>
+                </span>
+                <span className="story_separator">|</span>
+                <span>
+                  <a href="https://news.ycombinator.com/item?id=16582136">
+                    2 years ago
+                  </a>
+                </span>
+                <span className="story_separator">|</span>
+                <span>
+                  <a href="https://news.ycombinator.com/item?id=16582136">
+                    436 comments
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-footer text-muted">Pagination</div>
+      </div>
+    </div>
+  );
+};
 
 export default News;
