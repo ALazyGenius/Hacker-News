da625e4d4e8537bfa8bd43c01e48b8af62ca190d
vratika-goel <vratika.goel@hpe.com>
2020-05-30 16:06:35 +0530

    Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import HackerNewsLogo from '../assests/logo-hn-search-a822432b.jpg';
 
 const Header = () => {
 
@@ -7,16 +8,17 @@ const Header = () => {
     return (
         <div className="hacker-news-header">
             <div>
-                <img src="../assests/logo-hn-search-a822432b.jpg" alt="Hacker-search-logo" />
+                <img className="hacker-news-logo" src={HackerNewsLogo} alt="Hacker-search-logo" />
             </div>
-            <div class="input-group md-form form-sm form-1 pl-0">
+            <h5>Hacker News</h5>
+            <div class="input-group md-form form-sm form-1 pl-0 search-bar">
                 <div class="input-group-prepend">
-                    {/* <span class="input-group-text purple lighten-3" id="basic-text1"><i class="fas fa-search text-white"
-                        aria-hidden="true"></i></span> */}
                 </div>
-                <input class="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" />
+                <input class="form-control my-0 py-1" type="text" placeholder="Search stories by title, url or author" aria-label="Search" />
             </div>
-
+            <div className="header-settings">
+                <span className="settings">Settings</span>
+            </div>
 
         </div>
     );
