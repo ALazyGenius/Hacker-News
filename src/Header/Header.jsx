6d671a7117cd9404e0a95ebbc3dd0db8bd78936e
vratika-goel <vratika.goel@hpe.com>
2020-05-30 15:39:26 +0530

    Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,7 +9,14 @@ const Header = () => {
             <div>
                 <img src="../assests/logo-hn-search-a822432b.jpg" alt="Hacker-search-logo" />
             </div>
-            Headerrrrr
+            <div class="input-group md-form form-sm form-1 pl-0">
+                <div class="input-group-prepend">
+                    {/* <span class="input-group-text purple lighten-3" id="basic-text1"><i class="fas fa-search text-white"
+                        aria-hidden="true"></i></span> */}
+                </div>
+                <input class="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" />
+            </div>
+
 
         </div>
     );
